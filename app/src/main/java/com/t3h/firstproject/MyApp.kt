034fb45b8fb85d7c5ea48cc99a7b954def2454f1
Trung Doan <doanptand@gmail.com>
2021-03-14 10:18:59 +0700

    add log

--- app/src/main/java/com/t3h/firstproject/MyApp.kt
+++ app/src/main/java/com/t3h/firstproject/MyApp.kt
@@ -1,12 +1,13 @@
 package com.t3h.firstproject
 
 import android.app.Application
+import android.util.Log
 
 class MyApp : Application() {
     var account: Account? = null
 
     override fun onCreate() {
         super.onCreate()
-
+        Log.d("MyApp", "onCreate...........")
     }
 }
